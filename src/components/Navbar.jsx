3565f731faf38d00eb64ba74dbece764fd6d2e92
Rickson <ricksonreji0411@gmail.com>
2025-10-03 17:44:14 +0530

    Add full Car Rental App project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,17 @@ function AppNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-       
-        <Navbar.Brand as={Link} to="/"> QuickRental</Navbar.Brand>
-        
-      
+        {/* Brand */}
+        <Navbar.Brand as={Link} to="/">QuickRental</Navbar.Brand>
+
+        {/* Toggle for mobile view */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        
+
+        {/* Navigation Links */}
         <Navbar.Collapse id="basic-navbar-nav">
-        
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-           <Nav.Link href="/our-fleet">Our Fleet</Nav.Link>
-
+            <Nav.Link as={Link} to="/our-fleet">Our Fleet</Nav.Link>
             <Nav.Link as={Link} to="/booking">Booking</Nav.Link>
           </Nav>
         </Navbar.Collapse>
