c62b0adccfd8e900f43b40dd6fdd3d2564d82f4f
Rickson <ricksonreji0411@gmail.com>
2025-10-03 18:04:18 +0530

    Add full Car Rental App project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,37 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="bg-dark text-white mt-5 p-4">
       <Container>
         <Row>
+          {/* About Section */}
           <Col md={6}>
             <h5>QuickRental</h5>
             <p>Your trusted partner for car rentals. Luxury, affordable, and offroad vehicles at your fingertips.</p>
           </Col>
+
+          {/* Quick Links */}
           <Col md={3}>
             <h6>Quick Links</h6>
             <ul className="list-unstyled">
-              <li><a href="/" className="text-white text-decoration-none">Home</a></li>
-              <li><a href="/our-fleet" className="text-white text-decoration-none">Our Fleet</a></li>
-              <li><a href="/booking" className="text-white text-decoration-none">Booking</a></li>
+              <li><Link to="/" className="text-white text-decoration-none">Home</Link></li>
+              <li><Link to="/our-fleet" className="text-white text-decoration-none">Our Fleet</Link></li>
+              <li><Link to="/booking" className="text-white text-decoration-none">Booking</Link></li>
             </ul>
           </Col>
+
+          {/* Contact Info */}
           <Col md={3}>
             <h6>Contact Us</h6>
-            <p>Email: info@Quickrental.com</p>
+            <p>Email: info@QuickRental.com</p>
             <p>Phone: +91 9876543210</p>
           </Col>
         </Row>
-        <hr className="bg-white"/>
+
+        <hr className="bg-white" />
+
         <p className="text-center mb-0">&copy; 2025 QuickRental. All Rights Reserved.</p>
       </Container>
     </footer>
